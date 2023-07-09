9d57449ceaac6fdc1a8e33f15bf39999950f8efa
jamikrut <jamikrut@student.wszib.edu.pl>
2023-07-09 10:47:31 +0200

    Dodawanie kategorii

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,26 +1,29 @@
-import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
+import {NgModule} from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser';
 
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
-import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
-import { NavigationComponent } from './ui/navigation/navigation.component';
-import { CategoriesComponent } from './features/categories/categories.component';
-import { CategoryCardComponent } from './ui/category-card/category-card.component';
+import {AppRoutingModule} from './app-routing.module';
+import {AppComponent} from './app.component';
+import {FontAwesomeModule} from '@fortawesome/angular-fontawesome';
+import {NavigationComponent} from './ui/navigation/navigation.component';
+import {CategoriesComponent} from './features/categories/categories.component';
+import {CategoryCardComponent} from './ui/category-card/category-card.component';
+import {FormsModule} from "@angular/forms";
 
 @NgModule({
   declarations: [
     AppComponent,
     NavigationComponent,
     CategoriesComponent,
-    CategoryCardComponent
+    CategoryCardComponent,
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    FontAwesomeModule
+    FontAwesomeModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+}
